0440fb42e6d387bfe312a379958685eb74bbcc11
Narmstrongv <Narmstrongv@gmail.com>
2022-08-30 14:59:09 -0700

    changed the directory name from testTheme to Valley

--- header.php
+++ header.php
@@ -7,7 +7,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="Blog Site Template">
     <meta name="author" content="https://youtube.com/FollowAndrew">    
-    <link rel="shortcut icon" href="/wp-content/themes/testTheme/images/logo.png"> 
+    <link rel="shortcut icon" href="/wp-content/themes/Valley/images/logo.png"> 
 
 
 	<!-- FontAwesome CSS-->
